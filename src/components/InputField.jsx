8889d9e29b33aea9fa9f07987f57ddd66aa9f700
Vasyil Syman <vsyman9@gmail.com>
2024-05-17 18:33:33 +0300

    feat: add basic validation

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,14 +1,25 @@
 import { useState } from "react";
 
 const InputField = ({ children, value, onInputChange }) => {
+  const [showModal, setShowModal] = useState(false);
+
+  const validateInput = (e) => {
+    const input = e.target.value;
+
+    console.log(input);
+    if (parseFloat(input) >= 0) {
+      onInputChange(input);
+    } else {
+      onInputChange(0);
+      alert("Wrong number. Number must be >= 0");
+    }
+  };
+
   return (
     <div>
+      {showModal && <Modal />}
       <label>{children}</label>
-      <input
-        type="number"
-        value={value}
-        onChange={(e) => onInputChange(e.target.value)}
-      />
+      <input type="number" value={value} onChange={validateInput} min={0} />
     </div>
   );
 };
